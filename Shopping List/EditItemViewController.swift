f36c5c18d689143dc057d805946ba59be6596c44
johnnyparham <johnny.parham@gmail.com>
2016-05-11 13:56:27 -0400

    Add EditItemViewController

---
+++ Shopping List/EditItemViewController.swift
@@ -0,0 +1,64 @@
+//
+//  EditItemViewController.swift
+//  Shopping List
+//
+//  Created by Johnny Parham on 5/11/16.
+//  Copyright © 2016 Johnny Parham. All rights reserved.
+//
+
+import UIKit
+
+protocol EditItemViewControllerDelegate {
+    func controller(controller: EditItemViewController, didUpdateItem item: Item)
+}
+
+class EditItemViewController: UIViewController {
+
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var priceTextField: UITextField!
+    
+    var item: Item!
+    
+    var delegate: EditItemViewControllerDelegate?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Create Save Button
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Save, target: self, action: #selector(EditItemViewController.save(_:)))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: -
+    // MARK: Actions
+    func save(sender: UIBarButtonItem) {
+        if let name = nameTextField.text, let priceAsString = priceTextField.text, let price = Float(priceAsString) {
+            // Update Item
+            item.name = name
+            item.price = price
+            
+            // Notify Delegate
+            delegate?.controller(self, didUpdateItem: item)
+            
+            // Pop View Controller
+            navigationController?.popViewControllerAnimated(true)
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
